84089a40510062e60f351c98f93ff4b25ed8712c
viniciuspascucci1993 <vinicius.pascucci1@gmail.com>
2025-11-08 09:53:05 -0300

    chore: initial commit - Pix Service setup with Postgres container

---
+++ src/main/java/com/pixservice/PixServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pixservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pixservice/PixServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
